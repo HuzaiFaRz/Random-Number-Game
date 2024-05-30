4df9d2d6c9a42fe61325e673921aede567194ec6
HuzaiFa A <167712059+HuzaiFaRz@users.noreply.github.com>
2024-05-30 02:00:18 -0700

    some word added

--- app.js
+++ app.js
@@ -1,30 +1,32 @@
-var panelBottom = document.querySelector("#pbottom");
-var timer = document.querySelector(".timer");
-var timerValue = 60;
-var hits = document.querySelector(".hit");
-function makeBubble() {
-  var bubbles = "";
-  for (var i = 1; i <= 200; i++) {
-    var bubbleNumber = Math.floor(Math.random() * 10);
-    bubbles += `<div class="bubble">${bubbleNumber}</div>`;
-  }
-  panelBottom.innerHTML = bubbles;
-}
-makeBubble();
+// var gamebody = document.querySelector(".game_body");
 
-function timerRunner() {
-  var timerInt = setInterval(function () {
-    if (timerValue >= 0) {
-      timerValue--;
-      timer.textContent = timerValue;
-    } else {
-      timerInt = clearInterval();
-    }
-  }, 1000);
-}
-timerRunner();
-function hitsChanger() {
-  var hitRandom = Math.floor(Math.random() * 10);
-  hits.textContent = hitRandom;
-}
-hitsChanger();
+// var gametimer = document.querySelector("#game_timer");
+// var timerValue = 60;
+// var gamehit = document.querySelector("#game_hit");
+
+// function makeBubble() {
+//   var bubbles = "";
+//   for (var i = 1; i <= 200; i++) {
+//     var bubbleNumber = Math.floor(Math.random() * 10);
+//     bubbles += `<div class="bubble">${bubbleNumber}</div>`;
+//   }
+//   gamebody.innerHTML = bubbles;
+// }
+// makeBubble();
+
+// function timerRunner() {
+//   var timerInt = setInterval(function () {
+//     if (timerValue >= 0) {
+//       timerValue--;
+//       gametimer.textContent = timerValue;
+//     } else {
+//       timerInt = clearInterval();
+//     }
+//   }, 1000);
+// }
+// timerRunner();
+// function hitsChanger() {
+//   var hitRandom = Math.floor(Math.random() * 10);
+//   gamehit.textContent = hitRandom;
+// }
+// hitsChanger();
