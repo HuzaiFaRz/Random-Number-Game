a67dd0efb58054ab50143a8fc9ab72f41363c5c6
HuzaiFa A <167712059+HuzaiFaRz@users.noreply.github.com>
2024-05-31 06:13:52 -0700

    some

--- app.js
+++ app.js
@@ -1,38 +1,15 @@
-// var gameBody = document.querySelector(".game_body");
+var gameSignUpMain = document.querySelector(".game_sign_up");
+var gameSignUpForm = document.querySelector(".game_sign_up_form");
+var formError = document.querySelector(".form_error");
 
-// var gameTimer = document.querySelector("#game_timer");
-// var timerValue = 60;
-// var gameHit = document.querySelector("#game_hit");
-
-// function makeBubble() {
-//   var bubbles = "";
-//   for (var i = 1; i <= 200; i++) {
-//     var bubbleNumber = Math.floor(Math.random() * 10);
-//     bubbles += `<div class="bubble">${bubbleNumber}</div>`;
-//   }
-//   gameBody.innerHTML = bubbles;
-// }
-// makeBubble();
-
-// function timerRunner() {
-//   var timerInt = setInterval(function () {
-//     if (timerValue >= 0) {
-//       timerValue--;
-//       gameTimer.textContent = timerValue;
-//     } else {
-//       timerInt = clearInterval();
-//     }
-//   }, 1000);
-// }
-// timerRunner();
-// function hitsChanger() {
-//   var hitRandom = Math.floor(Math.random() * 10);
-//   gameHit.textContent = hitRandom;
-// }
-// hitsChanger();
-
+var game = document.querySelector(".game");
+var gameBody = document.querySelector(".game_body");
+var gameHit = document.querySelector("#game_hit");
+var gameTimer = document.querySelector("#game_timer");
+var gameScore = document.querySelector("#game_score");
 var signUpPasswordEye = document.querySelector(".sign_up_password_eye_hide");
 var signUpPassword = document.querySelector("#sign_up_password");
+
 function formPasswordShow() {
   try {
     if (signUpPasswordEye) {
@@ -51,16 +28,40 @@ function formPasswordShow() {
 }
 formPasswordShow();
 
-var signUpForm = document.querySelector(".game_sign_up_form");
-var formError = document.querySelector(".form_error");
-var userExist = false;
+function gameSignUpHide() {
+  gameSignUpMain.style.transition = "all 0.3s linear";
+  gameSignUpMain.style.visibility = "hidden";
+  gameSignUpMain.style.opacity = "0";
+  gameSignUpMain.style.zIndex = "-1111";
+}
+function gameSignUpShow() {
+  gameSignUpMain.style.transition = "all 0.3s linear";
+  gameSignUpMain.style.visibility = "visible";
+  gameSignUpMain.style.opacity = "1";
+  gameSignUpMain.style.zIndex = "1111";
+}
 
+function gameShow() {
+  game.style.transition = "all 0.3s linear";
+  game.style.visibility = "visible";
+  game.style.opacity = "1";
+  game.style.zIndex = "1111";
+}
+function gameHide() {
+  game.style.transition = "all 0.3s linear";
+  game.style.visibility = "hidden";
+  game.style.opacity = "0";
+  game.style.zIndex = "-1111";
+}
+
+var userExist = false;
 function signUp() {
-  var signUpFormData = new FormData(signUpForm);
+  var gameSignUpFormData = new FormData(gameSignUpForm);
   var signUpUserInfo = {
-    name: signUpFormData.get("name"),
-    email: signUpFormData.get("email"),
-    password: signUpFormData.get("password"),
+    name: gameSignUpFormData.get("name"),
+    email: gameSignUpFormData.get("email"),
+    password: gameSignUpFormData.get("password"),
+    score: gameScore.textContent,
   };
 
   if (
@@ -70,37 +71,75 @@ function signUp() {
   ) {
     formError.textContent = "Fill All Field Please";
     formError.style.color = "red";
-  } else if (signUpFormData.get("password").length < 8) {
+  } else if (gameSignUpFormData.get("password").length < 8) {
     formError.textContent = "Password Must Be 8 Least";
     formError.style.color = "red";
   } else {
-    if (
-      signUpUserInfo.email == existingUsers ||
-      signUpUserInfo.password == existingUsers
-    ) {
-      formError.textContent = "User Already Exist";
-      formError.style.color = "red";
-    } else {
-      userExist = true;
-      if (userExist) {
-        var existingUsers = JSON.parse(localStorage.getItem("Logged")) || [];
-        existingUsers.push(signUpUserInfo);
-        var newUser = localStorage.setItem(
-          "Logged",
-          JSON.stringify(existingUsers)
-        );
-        signUpForm.reset();
+    userExist = true;
+    if (userExist) {
+      var existingUsers = JSON.parse(localStorage.getItem("Logged")) || [];
+      existingUsers.push(signUpUserInfo);
+      var newUser = localStorage.setItem(
+        "Logged",
+        JSON.stringify(existingUsers)
+      );
+
+      if (
+        signUpUserInfo.email == newUser ||
+        signUpUserInfo.password == newUser
+      ) {
+        formError.textContent = "User Already Exist";
+        formError.style.color = "red";
+      } else {
+        gameSignUpForm.reset();
         formError.textContent = " Sign Up SuccessFully";
         formError.style.color = "#486848";
-      } else {
-        userExist = false;
+        setInterval(function () {
+          gameSignUpHide();
+        }, 2000);
+        setInterval(function () {
+          gameShow();
+        }, 3000);
       }
+    } else {
+      userExist = false;
     }
   }
 }
-if (signUpForm) {
-  signUpForm.addEventListener("submit", function (e) {
+if (gameSignUpForm) {
+  gameSignUpForm.addEventListener("submit", function (e) {
     e.preventDefault();
     signUp();
   });
 }
+
+function makeBubble() {
+  var bubbles = "";
+  for (var i = 1; i <= 170; i++) {
+    var bubbleNumber = Math.floor(Math.random() * 10);
+    bubbles += `<div class="bubble">${bubbleNumber}</div>`;
+  }
+  gameBody.innerHTML = bubbles;
+}
+makeBubble();
+var timerValue = 60;
+
+function timerRunner() {
+  if (gameShow()) {
+    var timerInt = setInterval(function () {
+      if (timerValue >= 0) {
+        timerValue--;
+        gameTimer.textContent = timerValue;
+      } else {
+        timerInt = clearInterval();
+      }
+    }, 1000);
+  }
+}
+timerRunner();
+
+function hitsChanger() {
+  var hitRandom = Math.floor(Math.random() * 10);
+  gameHit.textContent = hitRandom;
+}
+hitsChanger();
