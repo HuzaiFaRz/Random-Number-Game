e31845e979582208c7027eb41d0fcd8f4a78bf51
HuzaiFa A <167712059+HuzaiFaRz@users.noreply.github.com>
2024-06-02 07:28:46 +0500

    Game Make

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var main = document.querySelector(".main");
 var game = document.querySelector(".game");
 var gameBody = document.querySelector(".game_body");
 var gameHit = document.querySelector("#game_hit");
@@ -5,26 +6,46 @@ var gameTimer = document.querySelector("#game_timer");
 var gameScore = document.querySelector("#game_score");
 var againPlay = document.querySelector(".again-play");
 
+var gameScoreVal = 0;
+var bubbleNumber;
+var hitRandom = 0;
+var timerValue = 100;
+
+function scoreIncrease() {
+  gameScore.textContent = gameScoreVal += 10;
+  gameScore.style.color = "rgb(155, 184, 155)";
+}
+function scoreDecrase() {
+  gameScore.textContent = gameScoreVal -= 10;
+  gameScore.style.color = "red";
+}
+
 againPlay.addEventListener("click", function () {
   location.reload();
 });
 
 function makeBubble() {
   var bubbles = "";
-  // var gameBodyWidth = Math.floor(gameBody.clientWidth / bubbles);
-  for (var i = 1; i <= 100; i++) {
-    var bubbleNumber = Math.floor(Math.random() * 20);
+  for (var i = 1; i <= 341; i++) {
+    var bubbleNumber;
+    if (gameScoreVal > 150) {
+      bubbleNumber = Math.floor(Math.random() * 80);
+    } else if (gameScoreVal > 100) {
+      bubbleNumber = Math.floor(Math.random() * 50);
+    } else if (gameScoreVal > 50) {
+      bubbleNumber = Math.floor(Math.random() * 20);
+    } else if (gameScoreVal <= 50) {
+      bubbleNumber = Math.floor(Math.random() * 10);
+    }
     bubbles += `<div class="bubble">${bubbleNumber}</div>`;
-    gameBody.innerHTML = bubbles;
+    if (gameBody) {
+      gameBody.innerHTML = bubbles;
+    }
   }
-
-  console.log(Math.floor(gameBody.clientWidth / bubbles));
 }
 
 makeBubble();
-
-var timerValue = 60;
-
+var bubbledivs = document.querySelectorAll(".bubble");
 function timerRunner() {
   var timerInt = setInterval(function () {
     if (timerValue > 0) {
@@ -44,37 +65,53 @@ function timerRunner() {
 }
 timerRunner();
 
-var hitRandom = 0;
-
 function hitsChanger() {
-  hitRandom = Math.floor(Math.random() * 20);
+  if (gameScoreVal > 150) {
+    hitRandom = Math.floor(Math.random() * 80);
+  } else if (gameScoreVal > 100) {
+    hitRandom = Math.floor(Math.random() * 50);
+  } else if (gameScoreVal > 50) {
+    hitRandom = Math.floor(Math.random() * 20);
+  } else if (gameScoreVal <= 50) {
+    hitRandom = Math.floor(Math.random() * 10);
+  }
+
   gameHit.textContent = hitRandom;
 }
 hitsChanger();
 
-var gameScoreVal = 0;
-
-function scoreIncrease() {
-  gameScore.textContent = gameScoreVal += 10;
-  gameScore.style.color = "rgb(155, 184, 155)";
-}
-function scoreDecrase() {
-  gameScore.textContent = gameScoreVal -= 10;
-  gameScore.style.color = "red";
+function mainChangeColor() {
+  var randomColorCode = "#" + Math.floor(Math.random() * 580000);
+  main.style.backgroundColor = randomColorCode;
 }
 
-gameBody.addEventListener("click", function (t) {
-  var clickedNum = Number(t.target.textContent);
+gameBody.addEventListener("click", function (m) {
+  var clickedNum = Number(m.target.textContent);
   if (clickedNum === hitRandom) {
     scoreIncrease();
     hitsChanger();
     makeBubble();
+    gameScore.style.color = "rgb(155, 184, 155)";
+    mainChangeColor();
   } else {
-    scoreDecrase();
-    hitsChanger();
-    makeBubble();
+    gameScore.style.color = "red";
   }
 });
 
-var randomColorCode = Math.floor(Math.random() * 100000);
-// console.log(randomColorCode);
+// function gameStart() {
+//   Array.from(bubbledivs).forEach(function (t) {
+//     t.addEventListener("click", function (m) {
+//       var clickedNum = Number(m.target.textContent);
+//       if (clickedNum === hitRandom) {
+//         scoreIncrease();
+//         hitsChanger();
+//         makeBubble();
+//         gameScore.style.color = "rgb(155, 184, 155)";
+//         mainChangeColor();
+//       } else {
+//         gameScore.style.color = "red";
+//       }
+//     });
+//   });
+// }
+// gameStart();
